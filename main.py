e3b289a96f10b71bc65bfeeccc46f86a8d897d44
Kamran433 <102954239+Kamran433@users.noreply.github.com>
2022-11-02 01:23:05 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,24 @@
+# import random
+# import rock
+# import scissors
+# import paper
+# human=int(input("enter 0 for rock, 1 for paper and 2 for scissors:\n"))
+# computer=random.randint(0,2)
+# if human==0 and computer==1:
+#   print(f"Human:\n{rock.rock}\nComputer:\n{paper.paper}\nGAME OVER!")
+# elif human==0 and computer==2:
+#   print(f"Human:\n{rock.rock}\nComputer:\n{scissors.scissors}\nYOU WIN!")
+# elif human==1 and computer==0:
+#   print(f"Human:\n{paper.paper}\nComputer:\n{rock.rock}\nYOU WIN!")
+# elif human==1 and computer==2:
+#   print(f"Human:\n{paper.paper}\nComputer:\n{scissors.scissors}\nGAME OVER!")
+# elif human==2 and computer==0:
+#   print(f"Human:\n{scissors.scissors}\nComputer:\n{rock.rock}\nGAME OVER!")
+# elif human==2 and computer==1:
+#   print(f"Human:\n{scissors.scissors}\nComputer:\n{paper.paper}\nYOU WIN!")
+# elif human==0 and computer==0:
+#   print(f"Human:\n{rock.rock}\nComputer:\n{rock.rock}\nTRY AGAIN!")
+# elif human==1 and computer==1:
+#   print(f"Human:\n{paper.paper}\nComputer:\n{paper.paper}\nTRY AGAIN!")
+# elif human==2 and computer==2:
+#   print(f"Human:\n{scissors.scissors}\nComputer:\n{scissors.scissors}\nTRY AGAIN!")
